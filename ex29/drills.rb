edd9ce36bf15000ef690f74bcb6ea7ac9e62b8d1
Will Gant <w_gant@yahoo.co.uk>
2017-03-22 10:08:33 +0000

    Starting exercise 30 with empty file

---
+++ ex29/drills.rb
@@ -0,0 +1,15 @@
+if true
+    puts "1 is true!"
+end
+
+if true && false
+    puts "2 is true!"
+else
+    puts "2 is false!"
+end
+
+if !(10 < 5)
+    puts "3 is true"
+else
+    puts "3 is false"
+end
